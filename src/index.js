569c803771c5b6833fadf846fb17c523b50d2f1f
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-03-16 16:29:02 +0100

    Delete redux lines in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './utils/style/index.css';
 import App from './pages/App';
-import { Provider } from 'react-redux';
-import { store } from './redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
