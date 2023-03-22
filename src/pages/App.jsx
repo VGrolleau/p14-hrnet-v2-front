2baf9572daa43f736b35ae80b957c2e5bb655bbf
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-03-22 15:10:17 +0100

    Create Profile page|Update nav

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './SignIn';
 import CreateEmployee from './CreateEmployee';
 import EmployeeList from './EmployeeList';
 import Error from './Error';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<SignIn />} />
           <Route path="/create-employee" element={<CreateEmployee />} />
           <Route path="/employee-list" element={<EmployeeList />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
